e3074764f028bca31a7f777cc1b49af1a1553a10
Sanskar Rathore <114497305+sanskarrathore15@users.noreply.github.com>
2022-10-05 20:24:19 +0530

    For loop program
    Program to take input until user enter a negative number

---
+++ forloopnegative.c
@@ -0,0 +1,13 @@
+//WAP tp print values till the user enter a -ve value
+#include<stdio.h>
+int main()
+{int n;
+    for(n;n>0;n++)
+    {
+    
+      printf(" Enter any number ");
+      scanf("%d",&n);
+    }
+    printf("You entered a negative no.");
+return 0;
+}

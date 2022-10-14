208ad427325b1ac1e91be046f641c528295e6698
Sanskar Rathore <114497305+sanskarrathore15@users.noreply.github.com>
2022-10-14 22:41:33 +0530

    Helloworld.c

---
+++ Helloworld.c
@@ -0,0 +1,6 @@
+#include<stdio.h>    //hash is Preprocessor directive , include is keyword ,stdio standard input output 
+int main()                    //.h is extension for header file
+{
+    printf("Helloworld");
+return 0;
+}

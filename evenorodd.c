f9f9a77e71b8d570f529f89d4d8838d9150d34e1
Sanskar Rathore <114497305+sanskarrathore15@users.noreply.github.com>
2022-10-24 17:16:47 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int a,b;
+printf("Enter any number a ");
+scanf("%d",&a);
+if(a%2==0)
+{printf("%d is even",a);}
+else if(a%2==1)
+{printf("%d is odd",a);}
+
+return 0;
+}
